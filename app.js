8fce289d1eed108f04fd8745564009799c928945
Mohit Yadav <my701319@gmail.com>
2023-09-11 22:28:43 +0530

    Using CORs

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const express = require('express');
 const { join } = require('node:path');
 const connectDB = require('./db/connect');
 const authRoute = require('./routes/auth');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cors({
+  origin: "*",
+}));
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'public', 'html', 'signup.html'));
